87cda69bd8bc5cefd424909ca5eaa00169c231a2
Cevdet Sedef <cevdettsedeff@gmail.com>
2023-11-09 19:19:40 +0300

    Vote and Fav projections have been completed.

--- src/Projections/BlazorDictionary.Projections.FavoriteService/Worker.cs
+++ src/Projections/BlazorDictionary.Projections.FavoriteService/Worker.cs
@@ -1,21 +1,67 @@
+using BlazorDictionary.Common;
+using BlazorDictionary.Common.Events.Entry;
+using BlazorDictionary.Common.Events.EntryComment;
+using BlazorDictionary.Common.Infrastructure;
+
 namespace BlazorDictionary.Projections.FavoriteService
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            var connStr = _configuration.GetConnectionString("SqlConnection");
+
+            var favService = new Services.FavoriteService(connStr);
+
+            QueueFactory.CreateBasicConsumer()
+                .EnsureExchange(DictionaryConstants.FavExchangeName)
+                .EnsureQueue(DictionaryConstants.CreateEntryFavQueueName, DictionaryConstants.FavExchangeName)
+                .Receive<CreateEntryFavEvent>(fav =>
+                {
+                    favService.CreateEntryFav(fav).GetAwaiter().GetResult();
+                    _logger.LogInformation($"Received EntryId {fav.EntryId}");
+                })
+                .StartConsuming(DictionaryConstants.CreateEntryFavQueueName);
+
+            QueueFactory.CreateBasicConsumer()
+                .EnsureExchange(DictionaryConstants.FavExchangeName)
+                .EnsureQueue(DictionaryConstants.DeleteEntryFavQueueName, DictionaryConstants.FavExchangeName)
+                .Receive<DeleteEntryFavEvent>(fav =>
+                {
+                    favService.DeleteEntryFav(fav).GetAwaiter().GetResult();
+                    _logger.LogInformation($"Deleted Received EntryId {fav.EntryId}");
+                })
+                .StartConsuming(DictionaryConstants.DeleteEntryFavQueueName);
+
+            QueueFactory.CreateBasicConsumer()
+                .EnsureExchange(DictionaryConstants.FavExchangeName)
+                .EnsureQueue(DictionaryConstants.CreateEntryCommentFavQueueName, DictionaryConstants.FavExchangeName)
+                .Receive<CreateEntryCommentFavEvent>(fav =>
+                {
+                    favService.CreateEntryCommentFav(fav).GetAwaiter().GetResult();
+                    _logger.LogInformation($"Create EntryComment Received EntryCommentId {fav.EntryCommentId}");
+                })
+                .StartConsuming(DictionaryConstants.CreateEntryCommentFavQueueName);
+
+
+            QueueFactory.CreateBasicConsumer()
+                .EnsureExchange(DictionaryConstants.FavExchangeName)
+                .EnsureQueue(DictionaryConstants.DeleteEntryCommentFavQueueName, DictionaryConstants.FavExchangeName)
+                .Receive<DeleteEntryCommentFavEvent>(fav =>
+                {
+                    favService.DeleteEntryCommentFav(fav).GetAwaiter().GetResult();
+                    _logger.LogInformation($"Deleted Received EntryCommentId {fav.EntryCommentId}");
+                })
+                .StartConsuming(DictionaryConstants.DeleteEntryCommentFavQueueName);
         }
     }
 }
--- src/Projections/BlazorDictionary.Projections.VoteService/Worker.cs
+++ src/Projections/BlazorDictionary.Projections.VoteService/Worker.cs
@@ -1,21 +1,67 @@
+using BlazorDictionary.Common;
+using BlazorDictionary.Common.Events.Entry;
+using BlazorDictionary.Common.Events.EntryComment;
+using BlazorDictionary.Common.Infrastructure;
+
 namespace BlazorDictionary.Projections.VoteService
 {
     public class Worker : BackgroundService
     {
-        private readonly ILogger<Worker> _logger;
+        private readonly ILogger<Worker> logger;
+        private readonly IConfiguration configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
-            _logger = logger;
+            this.logger = logger;
+            this.configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            while (!stoppingToken.IsCancellationRequested)
-            {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
-            }
+            var connStr = configuration.GetConnectionString("SqlServer");
+
+            var voteService = new Services.VoteService(connStr);
+
+            QueueFactory.CreateBasicConsumer()
+                .EnsureExchange(DictionaryConstants.VoteExchangeName)
+                .EnsureQueue(DictionaryConstants.CreateEntryVoteQueueName, DictionaryConstants.VoteExchangeName)
+                .Receive<CreateEntryVoteEvent>(vote =>
+                {
+                    voteService.CreateEntryVote(vote).GetAwaiter().GetResult();
+                    logger.LogInformation("Create Entry Received EntryId: {0}, VoteType: {1}", vote.EntryId, vote.VoteType);
+                })
+                .StartConsuming(DictionaryConstants.CreateEntryVoteQueueName);
+
+            QueueFactory.CreateBasicConsumer()
+                .EnsureExchange(DictionaryConstants.VoteExchangeName)
+                .EnsureQueue(DictionaryConstants.DeleteEntryVoteQueueName, DictionaryConstants.VoteExchangeName)
+                .Receive<DeleteEntryVoteEvent>(vote =>
+                {
+                    voteService.DeleteEntryVote(vote.EntryId, vote.CreatedBy).GetAwaiter().GetResult();
+                    logger.LogInformation("Delete Entry Received EntryId: {0}", vote.EntryId);
+                })
+                .StartConsuming(DictionaryConstants.DeleteEntryVoteQueueName);
+
+
+            QueueFactory.CreateBasicConsumer()
+                    .EnsureExchange(DictionaryConstants.VoteExchangeName)
+                    .EnsureQueue(DictionaryConstants.CreateEntryCommentVoteQueueName, DictionaryConstants.VoteExchangeName)
+                    .Receive<CreateEntryCommentVoteEvent>(vote =>
+                    {
+                        voteService.CreateEntryCommentVote(vote).GetAwaiter().GetResult();
+                        logger.LogInformation("Create Entry Comment Received EntryCommentId: {0}, VoteType: {1}", vote.EntryCommentId, vote.VoteType);
+                    })
+                    .StartConsuming(DictionaryConstants.CreateEntryCommentVoteQueueName);
+
+            QueueFactory.CreateBasicConsumer()
+                    .EnsureExchange(DictionaryConstants.VoteExchangeName)
+                    .EnsureQueue(DictionaryConstants.DeleteEntryCommentVoteQueueName, DictionaryConstants.VoteExchangeName)
+                    .Receive<DeleteEntryCommentVoteEvent>(vote =>
+                    {
+                        voteService.DeleteEntryCommentVote(vote.EntryCommentId, vote.CreatedBy).GetAwaiter().GetResult();
+                        logger.LogInformation("Delete Entry Comment Received EntryCommentId: {0}", vote.EntryCommentId);
+                    })
+                    .StartConsuming(DictionaryConstants.DeleteEntryCommentVoteQueueName);
         }
     }
 }
